6f62400c01a6572c144acf50a2b5bba355a4f6b0
Jetross <jetrossneri07@gmail.com>
2026-01-13 01:19:43 +0800

    feat: add auth pages (login and register)

---
+++ app/(auth)/login/page.tsx
@@ -0,0 +1,9 @@
+export default function LoginPage() {
+  return (
+    <div className="min-h-screen">
+      <div className="flex justify-center items-center">
+        <h1 className="text-2xl font-semibold">Login Page</h1>
+      </div>
+    </div>
+  );
+}
---
+++ app/(auth)/register/page.tsx
@@ -0,0 +1,9 @@
+export default function RegisterPage() {
+  return (
+    <div className="min-h-screen">
+      <div className="flex justify-center items-center">
+        <h1 className="text-2xl font-semibold">Register Page</h1>
+      </div>
+    </div>
+  );
+}
