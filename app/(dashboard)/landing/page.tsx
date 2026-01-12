48afad903759d01aa8db193b9884b023739d27da
Jetross <jetrossneri07@gmail.com>
2026-01-13 01:14:04 +0800

    feat: add new page: landing

---
+++ app/(dashboard)/landing/page.tsx
@@ -0,0 +1,9 @@
+export default function LandingPage() {
+  return (
+    <div className="min-h-screen">
+      <div className="flex justify-center items-center">
+        <h1 className="text-2xl font-semibold">Welcome to the Landing Page</h1>
+      </div>
+    </div>
+  );
+}
