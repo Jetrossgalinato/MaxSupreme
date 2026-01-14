51e71ffa4aac1116eba621036eee05f4854d795d
Jetross <jetrossneri07@gmail.com>
2026-01-15 00:09:41 +0800

    fix: tw animate issue

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,7 @@
-const plugin = require("tw-animate-css");
-
 module.exports = {
   content: ["./app/**/*.{js,ts,jsx,tsx}"], // Adjust paths as needed
   theme: {
     extend: {},
   },
-  plugins: [plugin],
+  plugins: [],
 };
